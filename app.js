ff66db9b45fd793f958290490a3846a82e62cb15
alizeikh <muhammad.ali@zeikh.com>
2023-03-27 05:25:47 -0700

    backend

--- app.js
+++ app.js
@@ -19,8 +19,11 @@ const publicDirectory = path.join(__dirname, './public');
 app.use(express.static(publicDirectory));
 
 // Parse URL-encoded bodies (as sent by HTML forms)
+
 app.use(express.urlencoded({ extended: false }));
+
 // Parse JSON bodies (as sent by API clients)
+
 app.use(express.json());
 app.use(cookieParser());
 
@@ -35,6 +38,7 @@ db.connect( (error) => {
 })
 
 //Define Routes
+
 app.use('/', require('./routes/pages'));
 app.use('/auth', require('./routes/auth'));
 
